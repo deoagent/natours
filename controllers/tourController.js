3aa9c42a54a5c4a0e785579d628d153e58bbf0ff
Deo <d.trinidad@lbteksystems.com>
2023-08-29 23:22:51 +0800

    Making the API Better Limiting Fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,6 +24,14 @@ exports.getAlltours = async (req, res) => {
       query.sort('-createdAt');
     }
 
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
 
