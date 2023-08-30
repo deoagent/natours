7fcb56216e766a6b2fd6bb461fceeaeb3f437288
Deo <d.trinidad@lbteksystems.com>
2023-08-30 23:12:06 +0800

    Making the API Better Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -32,8 +32,22 @@ exports.getAlltours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10, page 1, 11-20, page 2,
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE THE QUERY
     const tours = await query;
+    // query.sort().select().skip().limit()
 
     // const query = await Tour.find()
     //   .where('duration')
