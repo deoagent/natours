3dee57b6a961fd856f863935f434514b9dc2bac2
Deoniño Trinidad <deo@lbteksystems.com>
2023-09-07 21:09:24 +0800

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 // 4) START THE SERVER
 
 module.exports = app;
